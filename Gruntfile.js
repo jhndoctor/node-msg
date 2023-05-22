dd0470273f182a0753e5468ba24c613a1cbf35d9
jhndoctor <johndoctor269@gmail.com>
2023-05-22 14:03:42 +0630

    add all the things

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+const sass = require('node-sass')
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        sass: {
+            dist: {
+                files: {
+                    'public/stylesheets/application.css' : 'sass/application.scss'
+                }
+            },
+            options: {
+                implementation: sass,
+                sourceMap: true,
+                includePaths: [
+                    require('node-bourbon').includePaths,
+                    './bower_components/color-scale',
+                    './bower_components/type-rhythm-scale'
+                ]
+            }
+        },
+        watch: {
+            source: {
+                files: ['sass/**/*.scss', 'views/**/*.jade'],
+                tasks: ['sass'],
+                options: {
+                    livereload: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch')
+    grunt.loadNpmTasks('grunt-sass')
+    grunt.registerTask('default', ['sass'])
+}
